d5f08c3d6fad914c7440634b5a27706d8e9fe7a0
gragra <gragra02@gmail.com>
2024-11-11 19:35:39 -0300

    atualização da trilha

---
+++ 01 -  Estrutura de dados/01 - Listas/00_delarando_listas.py
@@ -0,0 +1,11 @@
+frutas = ["maçã","laranja", "abacate", "melancia"]
+frutas=[-1]
+frutas = [-3]
+
+
+listas = ["A", "B", "C", "D", ]
+listas[:2]
+print(len(listas))
+
+for lista in listas:
+    print(listas)         
